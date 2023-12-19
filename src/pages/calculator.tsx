6f1d902097bb6af3fa46a5999e4d3e7912eab823
TsukasaShirai <whitewells0914@gmail.com>
2023-12-19 20:49:08 +0900

    演算子入力後「＝」連打で第二項を用いた演算を繰り返す、第二項入力なしで、演算子入力して「＝」連打で、初期第一項を第二項と見做して演算を繰り返す、という電卓機能を追加

--- src/pages/calculator.tsx
+++ src/pages/calculator.tsx
@@ -1,62 +1,113 @@
-import {promises} from 'fs';
-import { GetStaticProps, NextPage } from 'next';
-import * as path from 'path';
-import * as process from 'process';
+import { NextPage } from 'next';
 import React, { ReactElement, useState } from 'react';
 import { Button } from '../components/Button';
 
-type Country = {
-  jpnName: string;
-  engName: string;
-  numeric: number;
-  alpha3: string;
-  alpha2: string;
-  location: string;
-  subDivision: string;
-};
+const IndexPage: NextPage = (): ReactElement => {
 
-type Props = {
-  countries: Array<Country>;
-};
+  //カレントの数値
+  const [display, setDisplay] = useState<string>("0");
+  //前項
+  const [term1, setTerm1] = useState<number>(0);
+  //後項
+  const [term2, setTerm2] = useState<number>(0);
+  //演算子
+  const [operator, setOperator] = useState<string>("");
+  //演算記号入力
+  const [isOperated, setOperated] = useState<boolean>(false);
 
-const IndexPage: NextPage<Props> = (): ReactElement => {
-  const [count, setCount] = useState<number>(0);
+  const clear =(): void => {
+    setDisplay("0");
+    setTerm1(0);
+    setTerm2(0);
+    setOperator("");
+    setOperated(false);
+  }
+
+  //数値入力
+  const input = (value: string): string => {
+    
+    let result:string = display; 
+    if(isOperated){
+      setOperated(false);
+      result = "0";
+    }
+    
+    if(value == "."){
+      if(display.indexOf(".") == -1 ){
+        result += ".";
+      }
+    }else if (result == "0") {
+      result = value;
+    }else{
+      result += value;
+    } 
+    setTerm2(Number(result));
+    return result;
+  };
+
+  //演算子入力
+  const operate = (oprator: string): void=> {
+    setOperated(true);
+    setOperator(oprator);
+    setTerm1(Number(display));
+    setTerm2(Number(display));
+  };
+
+  //計算
+  const calc = (): string => {
+    
+    let result:number = Number(display);
+    switch(operator){
+      case "+":
+        result = term1 + term2;
+        break;
+      case "-":
+        result = term1 - term2;
+        break;
+      case "/":
+        result = term1 / term2;
+        break;
+      case "*":
+        result = term1 * term2;
+        break;
+    }
+    setTerm1(result);
+    return String(result);
+  };
 
   return (
     <>
       <div className="m-10 p-4 w-2/3 mx-auto shadow-lg border-4 rounded-2xl">
         <div className="mx-auto">
           <div className="p-3 mb-3 border-4 rounded h-full w-full text-right">
-            <span className="text-gray-700 select-none">{count}</span>
+            <span className="text-gray-700 select-none">{display}</span>
           </div>
           <div className="grid grid-cols-4 gap-2">
             <Button
               className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                console.log(count);
-                setCount(count + 1);
+                setDisplay(input("7"));
               }}>
               <span className="select-none text-xl">7</span>
             </Button>
             <Button
               className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                console.log(count);
-                setCount(count - 1);
+                setDisplay(input("8"));
               }}>
               <span className="select-none text-xl">8</span>
             </Button>
             <Button
               className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                setCount(0);
+                setDisplay(input("9"));
               }}>
               <span className="select-none text-xl">9</span>
             </Button>
             <Button
               className="py-2 bg-pink-300 hover:bg-pink-400 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                setCount(0);
+                operate("/");
               }}>
               <span className="select-none text-xl">/</span>
             </Button>
@@ -65,100 +116,88 @@ const IndexPage: NextPage<Props> = (): ReactElement => {
             <Button
               className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                console.log(count);
-
-                setCount(count + 1);
+                setDisplay(input("4"));
               }}>
               <span className="select-none text-xl">4</span>
             </Button>
             <Button
               className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                console.log(count);
-
-                setCount(count - 1);
+                setDisplay(input("5"));
               }}>
               <span className="select-none text-xl">5</span>
             </Button>
             <Button
               className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                setCount(0);
+                setDisplay(input("6"));
               }}>
               <span className="select-none text-xl">6</span>
             </Button>
             <Button
               className="py-2 bg-pink-300 hover:bg-pink-400 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                setCount(0);
+                operate("*");
               }}>
               <span className="select-none text-xl">*</span>
             </Button>
           </div>
           <div className="grid grid-cols-4 gap-2 my-2">
             <Button
-              className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+              className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                console.log(count);
-
-                setCount(count + 1);
+                setDisplay(input("1"));
               }}>
               <span className="select-none text-xl">1</span>
             </Button>
             <Button
-              className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+              className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                console.log(count);
-
-                setCount(count - 1);
+                setDisplay(input("2"));
               }}>
               <span className="select-none text-xl">2</span>
             </Button>
             <Button
-              className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+              className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                setCount(0);
+                setDisplay(input("3"));
               }}>
               <span className="select-none text-xl">3</span>
             </Button>
             <Button
               className="py-2 bg-pink-300 hover:bg-pink-400 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                setCount(0);
+                operate("-");
               }}>
               <span className="select-none text-xl">-</span>
             </Button>
           </div>
           <div className="grid grid-cols-4 gap-2 my-2">
             <Button
-              className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+              className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                console.log(count);
-
-                setCount(count + 1);
+                setDisplay(input("."));
               }}>
               <span className="select-none text-xl">.</span>
             </Button>
             <Button
-              className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+              className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                console.log(count);
-
-                setCount(count - 1);
+                setDisplay(input("0"));
               }}>
               <span className="select-none text-xl">0</span>
             </Button>
             <Button
-              className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+              className="py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                setCount(0);
+                setDisplay(calc());
               }}>
               <span className="select-none text-xl">=</span>
             </Button>
             <Button
               className="py-2 bg-pink-300 hover:bg-pink-400 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                setCount(0);
+                operate("+");
               }}>
               <span className="select-none text-xl">+</span>
             </Button>
@@ -167,9 +206,7 @@ const IndexPage: NextPage<Props> = (): ReactElement => {
             <Button
               className="py-2 bg-teal-400 hover:bg-teal-500 text-white rounded border border-gray-200 cursor-pointer"
               onClick={() => {
-                console.log(count);
-
-                setCount(count + 1);
+                clear();
               }}>
               <span className="select-none text-xl">C</span>
             </Button>
@@ -180,16 +217,7 @@ const IndexPage: NextPage<Props> = (): ReactElement => {
   );
 };
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const buffer = await promises.readFile(path.join(process.cwd(), 'json', 'countries.json'));
-  const str  = buffer.toString();
 
-  return {
-    props: {
-      countries: JSON.parse(str)
-    }
-  };
-};
 
 // eslint-disable-next-line import/no-default-export
 export default IndexPage;
